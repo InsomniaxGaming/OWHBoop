3b97fa1d6eaa7aae02061be8a6a720ea90d7cdf8
Insomniac10102 <ins10102@gmail.com>
2014-03-10 02:14:12 -0700

    Forked OWHBlank

---
+++ src/info/insomniax/owhblank/core/BukkitPlugin.java
@@ -0,0 +1,36 @@
+package info.insomniax.owhblank.core;
+
+import info.insomniax.owhblank.vault.Permissions;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * This class acts as a bridge between the plugin and the server software. When possible, anything that involves
+ * referencing bukkit should have to pass through this plugin to make for optimal portability.
+ * */
+public class BukkitPlugin extends JavaPlugin
+{
+	
+	Permissions p;
+	
+	public void onEnable()
+	{
+		//TODO setup plugin.yml
+		
+		p = new Permissions(this);
+		p.setupPermissions();
+	}
+	
+	public void onDisable()
+	{
+		this.saveConfig();
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+	{
+		return false;
+	}
+
+}
